67334f6586e04b30772e06bfc9a80ffe03842ea9
FrancescaBambozzi <bambozzif@gmail.com>
2023-11-07 18:05:59 +0100

    added privacy and hamburger menu

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-let currentSlide = 0;
-const slides = document.querySelectorAll('.carousel-slide');
-
-function showSlide(slideIndex) {
-    slides.forEach((slide, index) => {
-        if (index === slideIndex) {
-            slide.style.display = 'block';
-        } else {
-            slide.style.display = 'none';
-        }
-    });
-}
-
-function nextSlide() {
-    currentSlide = (currentSlide + 1) % slides.length;
-    showSlide(currentSlide);
-}
-
-// Automatically advance the carousel every 5 seconds
-setInterval(nextSlide, 5000);
+document.getElementById('menu-toggle').addEventListener('click', function () {
+    document.getElementById('nav-dialog').classList.toggle('active');
+});
